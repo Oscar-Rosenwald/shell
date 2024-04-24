130491f7370bfc2591a989de83ab0acfd42d41e3
Cyril <cyril.saroch@ava.uk>
2024-04-24 13:04:52 +0100

    get_cookie: Add ability to decrypt cookies encyrpted by various methods

--- get_cookie.py
+++ get_cookie.py
@@ -1,15 +1,25 @@
 # I got this mostly from the internet. Chrome stores cookies in a Cookie file
 # under the Chrome profile directory. Cookies are encrypted using a password
-# which is available online. Here I find the cookie for the VMS I want to
-# access. This only works as long as I'm logged in in the browser.
+# which is available online, or possibly needs to be found in the keychain(?).
+# Here I find the cookie for the VMS I want to access. This only works as long
+# as I'm logged in in the browser.
 
 from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import PBKDF2
-from subprocess import PIPE, Popen
 from sqlite3 import dbapi2
 import sys
 
-def clean(x): 
+
+def decrypt_with_key(encrypted_value, key, salt, iv, length, iterations):
+    my_pass = key.encode('utf8')
+
+    key = PBKDF2(my_pass, salt, length, iterations)
+    cipher = AES.new(key, AES.MODE_CBC, IV=iv)
+
+    decrypted = cipher.decrypt(encrypted_value)
+    return clean(decrypted)
+
+def clean(x):
     return x[:-x[-1]].decode('utf8').replace("\x08", "")  # Remove backspace characters
 
 def decrypt(encrypted_value):
@@ -19,22 +29,18 @@ def decrypt(encrypted_value):
     # Default values used by both Chrome and Chromium in OSX and Linux
     salt = b'saltysalt'
     iv = b' ' * 16
-    length = 16
+    length = 16 
+    iterations = 1  # 1003 on Mac, 1 on Linux
 
-    # I got this from the Seahorse application. Google uses this password to
-    # encrypt cookies in the Cookies database.
-    # key = "R1LVz6+VIq4eGyCEm6wqYg=="
-    key = 'peanuts'
-    my_pass = key.encode('utf8')
+    try:
+        # This is the old password which Google still uses sometimes.
+        key = 'peanuts'
+        return decrypt_with_key(encrypted_value, key, salt, iv, length, iterations)
+    except UnicodeDecodeError:
+        # This is the new password, retrieved from seahorse.
+        key = 'R1LVz6+VIq4eGyCEm6wqYg=='
+        return decrypt_with_key(encrypted_value, key, salt, iv, length, iterations)
 
-    # 1003 on Mac, 1 on Linux
-    iterations = 1
-
-    key = PBKDF2(my_pass, salt, length, iterations)
-    cipher = AES.new(key, AES.MODE_CBC, IV=iv)
-
-    decrypted = cipher.decrypt(encrypted_value)
-    return clean(decrypted)
 
 database = "/home/lightningman/.config/google-chrome/Profile 3/Cookies"
 sql = 'SELECT name, encrypted_value FROM cookies WHERE host_key like "%%%s%%"' % sys.argv[1]
@@ -52,5 +58,5 @@ for name, enc_val in rows:
 
 if "va" not in cookies:
     print("No cookie found. Are you logged in to the VMS UI?")
-else: 
+else:
     print(cookies["va"])
