9b43aaf48fc7a04c56653143119c1509547605a2
Cyril <cyril.saroch@ava.uk>
2024-08-31 10:27:18 +0100

    load-shell: Update to reflect macOS merger

--- load-shell.sh
+++ load-shell.sh
@@ -2,32 +2,43 @@
 
 # This script loads all variables, functions, and completions from the shell
 # repository. Use `source <this-file>` in your .bashrc.
+#
+# Always source .bash_variables before this script.
 
-export PRIVATE_DIR=~/Private/
-export MAPS=$PRIVATE_DIR/cache/mappings
-export CCs=$PRIVATE_DIR/cache/CCs
-export SHELL_DIR=~/shell
-export COMMON_DIR=$SHELL_DIR/common
-export WORK_DIR=$SHELL_DIR/work
-export HOME_DIR=$SHELL_DIR/home
-export UTILS_DIR=$WORK_DIR/utils
-export WORK_COMPUTER=
-export COMPONENTS=
+if [[ $WORK_COMPUTER = true ]]; then
+	[[ -f $WORK_DIR/resiliency_functions ]] && . $WORK_DIR/resiliency_functions
+	[[ -f $WORK_DIR/cluster ]] && . $WORK_DIR/cluster
 
-if [[ $(whoami) = ncx843 ]]; then
-	WORK_COMPUTER=true
-	COMPONENTS="mgmt db access norm streamer ui authenticator store router"
+	if [[ -d $VOLTA_HOME ]]; then
+		PATH="$VOLTA_HOME/bin:$PATH"
+		PATH=$BUN_INSTALL/bin:$PATH
+	fi
+
+	[[ -d $HOME/GoLand/bin/ ]] && PATH="$PATH:~/GoLand/bin/"
+	[[ -d $VAION_PATH/scripts ]] && PATH="$PATH:$VAION_PATH/scripts:$VAION_PATH/scripts/ha"
+	[[ -d $VAION_PATH/../toolset/scripts/ ]] && PATH="$PATH:$VAION_PATH/../toolset/scripts"
+	[[ -d /usr/bin ]] && PATH="/usr/bin:$PATH"
+else
+	export VISUAL="vim"
+	if [[ -d $HOME_DIR ]]; then
+		. $HOME_DIR/filmList
+		. $HOME_DIR/bookList
+		. $HOME_DIR/screenshot
+		. $HOME_DIR/phones
+		. $HOME_DIR/webpgif
+		. $HOME_DIR/memoryOut
+		. $HOME_DIR/inspiration
+		. $HOME_DIR/set-background
+	fi
+	if [[ -d $MAGIC_DIR ]]; then
+		PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/homebrew/bin:/Library/TeX/texbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:$MAGIC_DIR/scripts
+	fi
 fi
 
 # Load common scripts
 [[ -f $COMMON_DIR/updir_func ]] && . $COMMON_DIR/updir_func
 [[ -f $COMMON_DIR/completion.bash ]] && . $COMMON_DIR/completion.bash
 
-# Load computer-specific scipts.
-if [[ $WORK_COMPUTER = true ]]; then
-	[[ -f $WORK_DIR/resiliency_functions ]] && . $WORK_DIR/resiliency_functions
-	[[ -f $WORK_DIR/cluster ]] && . $WORK_DIR/cluster
-fi
 
 function addDirToPath() {
 	topLevelDir=$1
@@ -35,7 +46,7 @@ function addDirToPath() {
 		# We want to avoid adding work/utils/ to PATH. It's a place that
 		# contains scripts which are only called by other scripts, and we
 		# therefore don't want them to clutter up completion.
-		if [[ -d $fileOrDir && $(realpath $fileOrDir) != $WORK_DIR/utils ]]; then
+		if [[ -d $fileOrDir && $(realpath $fileOrDir) != $UTILS_DIR ]]; then
 			PATH="$PATH:$fileOrDir"
 			addDirToPath $fileOrDir
 		fi
